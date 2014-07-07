24665375dcab9586146c581b73b02215932c1f22
howlingfantods47 <neelshah@ist.ac.at>
2014-07-07 17:02:14 +0200

    LSIS buggy

--- LSIS.cpp
+++ LSIS.cpp
@@ -68,15 +68,41 @@ void max_function_lsis(int current, double* w, double* a, void* user_arg){
 			g -> add_tweights(i, 0, E0-E1);
 		}
 	}
-	int i,j;
+	int p,q;
 	for(int e=0; e<m; ++e){
-		i = cur_sample.edges[e].s;
-		j = cur_sample.edges[e].t;
-		g -> add_tweights(i, 1, 0);
-		g -> add_tweights(j, 0, 1);
-		g -> add_edge(i, j, 2, 0);
+		p = cur_sample.edges[e].s;
+		q = cur_sample.edges[e].t;
+		g -> add_tweights(p, 1, 0);
+		g -> add_tweights(q, 0, 1);
+		g -> add_edge(p, q, 2, 0);
 	}
 	int flow = g -> maxflow();
+	for(int i=0; i<dimension+1; ++i){
+		a[i] = 0;
+	}
+	for(int i=0; i<n; ++i){
+		if(cur_sample.labels[i] == 0 && g -> what_segment(i) == 1){
+			for(int j=0; j<sp_dim; ++j){
+				a[j] += cur_sample.features[i][j];
+				a[sp_dim + j] -= cur_sample.features[i][j];
+			}
+		}
+		else if(cur_sample.labels[i] == 1 && g -> what_segment(i) == 0){
+			for(int j=0; j<sp_dim; ++j){
+				a[j] -= cur_sample.features[i][j];
+				a[sp_dim + j] += cur_sample.features[i][j];
+			}
+		}
+	}
+	double loss = 0;
+	for(int i=0; i<n; ++i){
+		if(cur_sample.labels[i] != g -> what_segment(i)){
+			loss += 1.0;
+		}
+	}
+	loss = loss/n;
+	a[dimension] = loss;
+	delete g;
 }
 
 
@@ -152,45 +178,33 @@ void read_lsis(string filename, vector<Sample>& data){
 }
 
 int main(int argc, char* argv[]){
-	/*      string filename;
-			  int group_size;
-			  int iter_max, cutting_planes_max, inner_iter;
-			  if(argc != 6){
-			  cout << "Usage: " << argv[0] << " train_file group_size iter_max cutting_planes_max inner_iter_max (group_size = +ve integer or -1 for simple Frank_Wolfe)" << endl;
-			  return 1;
-			  }
-			  else{
-			  filename = argv[1];
-			  group_size = atoi(argv[2]);
-			  iter_max = atoi(argv[3]);
-			  cutting_planes_max = atoi(argv[4]);
-			  inner_iter = atoi(argv[5]);
+	int group_size;
+	int iter_max, cutting_planes_max, inner_iter;
+	if(argc != 5){
+		cout << "Usage: " << argv[0] << " group_size iter_max cutting_planes_max inner_iter_max (group_size = +ve integer or -1 for simple Frank_Wolfe)" << endl;
+		return 1;
+	}
+	else{
+		group_size = atoi(argv[1]);
+		iter_max = atoi(argv[2]);
+		cutting_planes_max = atoi(argv[3]);
+		inner_iter = atoi(argv[4]);
 
-			  }*/
+	}
 	read_lsis("datasets/LSIS/", samples);
 	dimension = 2*sp_dim;
 	num_train = samples.size();
-	double* w = new double[dimension];
 	double lambda = 1;
-	for(int i=0; i<dimension/2; ++i){
-		w[i] = 1;
-	}
-	for(int i=dimension/2; i<dimension; ++i){
-		w[i] = 0;
-	}
-	double* a = new double[dimension+1];
-	max_function_lsis(0, w, a, NULL);
-	delete a;
-	delete w;
-	/*  if(group_size == -1)    group_size = num_train;
-	SVM svm_ocr(dimension, num_train, lambda, max_function_ocr, NULL, NULL, group_size);
+	if(group_size == -1)    group_size = num_train;
+	SVM svm_ocr(dimension, num_train, lambda, max_function_lsis, NULL, NULL, group_size);
 	svm_ocr.options.iter_max = iter_max;
 	svm_ocr.options.cutting_planes_max = cutting_planes_max;
 	svm_ocr.options.inner_iter_max = inner_iter ;
 	svm_ocr.options.callback_freq = 1;
 	double* w_opt;
 	cout << "time_before_bound   iteration   lower_bound    upper_bound    duality_gap    time_after_bound" <<endl;
-	w_opt = svm_ocr.Solve();*/
+	w_opt = svm_ocr.Solve();
+	delete w_opt;
 	return 0;
 }
 
